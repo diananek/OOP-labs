f88f6baac99b17acc25d8cf61edd436ca30d77d2
Diana Nekrasova <ya.nel2014@ya.ru>
2022-10-26 14:23:28 +0300

    Добавлена возможность писать любые символы

--- lab5/task3/DeleteDuplicates.java
+++ lab5/task3/DeleteDuplicates.java
@@ -1,6 +1,7 @@
 package lab5.task3;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -8,20 +9,9 @@ import java.util.Scanner;
 public class DeleteDuplicates {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        List<Integer> inputArray = stringToIntArray(input.nextLine());
-        HashSet<Integer> resultArray = new HashSet<Integer>(inputArray);
+        String inputLine = input.nextLine();
+        List inputArray = Arrays.asList(inputLine.split(" "));
+        HashSet resultArray = new HashSet<>(inputArray);
         System.out.println(resultArray);
-    }
-    public static List<Integer> stringToIntArray(String line) {
-        String[] stringArray = line.split(",");
-
-        List<Integer> intArray = new ArrayList<Integer>();
-
-        for (int i = 0; i < stringArray.length; i++) {
-            intArray.add(Integer.valueOf(stringArray[i]));
-        }
-
-        return intArray;
     }
 }
